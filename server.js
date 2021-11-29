dde27a8d381952e96f73efa52ddd6ceec2e924c4
mahurju <marucool@naver.com>
2021-11-29 10:58:42 +0900

    🚿🎮 Updated with Glitch (#2)
    Co-authored-by: Glitch (engoo-noti) <none>

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.static('public'));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+console.log('running');
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
